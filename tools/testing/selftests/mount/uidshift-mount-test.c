bac862b70072045ecca10dbe3c41717783203cc2
Djalal Harouni <djalal@endocode.com>
2015-06-27 00:16:20 +0100

    uidshift: test: add a skeleton test for our usecase
    Signed-off-by: Djalal Harouni <djalal@endocode.com>

---
+++ tools/testing/selftests/mount/uidshift-mount-test.c
@@ -0,0 +1,374 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/mount.h>
+#include <sys/prctl.h>
+#include <sys/wait.h>
+#include <sys/vfs.h>
+#include <sys/statvfs.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <sys/syscall.h>
+#include <sys/eventfd.h>
+#include <linux/sched.h>
+
+#ifndef CLONE_NEWUSER
+# define CLONE_NEWUSER 0x10000000
+#endif
+
+static int efd;
+static int efd_userns_child;
+
+static int vmaybe_write_file(bool enoent_ok, char *filename, char *fmt, va_list ap)
+{
+	char buf[4096];
+	int fd;
+        int ret;
+	ssize_t written;
+	int buf_len;
+
+	buf_len = vsnprintf(buf, sizeof(buf), fmt, ap);
+	if (buf_len < 0) {
+                ret = -errno;
+		printf("vsnprintf failed: %d (%m)\n", ret);
+                return ret;
+	}
+	if (buf_len >= sizeof(buf)) {
+                ret = -1;
+		printf("vsnprintf output truncated\n");
+                return ret;
+	}
+
+	fd = open(filename, O_WRONLY);
+	if (fd < 0) {
+		if ((errno == ENOENT) && enoent_ok)
+			return 0;
+                ret = -errno;
+		printf("open of %s failed: %d (%m)\n",
+		        filename, ret);
+                return ret;
+	}
+	written = write(fd, buf, buf_len);
+	if (written != buf_len) {
+		if (written >= 0) {
+			printf("short write to %s\n", filename);
+                        return -1;
+		} else {
+                        ret = -errno;
+			printf("write to %s failed: %d (%m)\n",
+				filename, ret);
+                        return ret;
+		}
+	}
+	if (close(fd) != 0) {
+                ret = -errno;
+		printf("close of %s failed: %d (%m)\n",
+			filename, ret);
+                return ret;
+	}
+
+        return 0;
+}
+
+static int maybe_write_file(char *filename, char *fmt, ...)
+{
+        int ret;
+	va_list ap;
+
+	va_start(ap, fmt);
+	ret = vmaybe_write_file(true, filename, fmt, ap);
+	va_end(ap);
+
+        return ret;
+}
+
+static int write_file(char *filename, char *fmt, ...)
+{
+        int ret;
+	va_list ap;
+
+	va_start(ap, fmt);
+	ret = vmaybe_write_file(false, filename, fmt, ap);
+	va_end(ap);
+
+        return ret;
+}
+
+static int setup_userns(pid_t pid)
+{
+        int ret;
+        char buf[64];
+
+        snprintf(buf, sizeof(buf), "/proc/%d/setgroups", pid);
+	ret = maybe_write_file(buf, "deny");
+        if (ret < 0)
+                goto err;
+
+        snprintf(buf, sizeof(buf), "/proc/%d/uid_map", pid);
+	ret = write_file(buf, "0 1000 65534 \n");
+        if (ret < 0)
+                goto err;
+
+        snprintf(buf, sizeof(buf), "/proc/%d/gid_map", pid);
+	ret = write_file(buf, "0 1000 65534 \n");
+        if (ret < 0)
+                goto err;
+
+        return 0;
+
+err:
+        printf("setting up the user namespace failed\n");
+        return ret;
+}
+
+static int drop_to_user(void)
+{
+        int ret;
+
+        printf("getuid: %d\n", getuid());
+
+        ret = setgroups(0, NULL);
+        if (ret < 0) {
+                ret = -errno;
+                printf("setgroups() failed: %d (%m)\n", ret);
+                return ret;
+        }
+
+        ret = setresgid(1000, 1000, 1000);
+        if (ret < 0) {
+                ret = -errno;
+                printf("setresgid() failed: %d (%m)\n", ret);
+                return ret;
+        }
+
+        ret = setresuid(1000, 1000, 1000);
+        if (ret < 0) {
+                ret = -errno;
+                printf("setresuid() failed: %d (%m)\n", ret);
+                return ret;
+        }
+
+        return 0;
+}
+
+static int setup_and_test_procfs(void)
+{
+        int ret;
+
+        ret = mount("/proc", "/proc", "bind", MS_BIND, NULL);
+        if (ret < 0) {
+                ret = -errno;
+                printf("mount() failed: %d (%m)\n", ret);
+                return ret;
+        }
+
+        /* TODO stat proc inode entries... */
+
+
+        return 0;
+}
+
+static int outer_child(void)
+{
+        int ret;
+        int status;
+        pid_t pid, rpid;
+	eventfd_t event_status = 0;
+
+        pid = syscall(__NR_clone, SIGCHLD|
+                      CLONE_NEWNS|CLONE_NEWUSER, NULL);
+        if (pid < 0) {
+                ret = -errno;
+		printf("clone() failed: %d (%m)\n", -errno);
+		return ret;
+        }
+
+        if (pid == 0) {
+		ret = prctl(PR_SET_PDEATHSIG, SIGKILL);
+		if (ret < 0) {
+			ret = -errno;
+			printf("error prctl(): %d (%m)\n", ret);
+			_exit(EXIT_FAILURE);
+		}
+
+		ret = eventfd_read(efd_userns_child, &event_status);
+		if (ret < 0 || event_status != 1) {
+			printf("error eventfd_read() *** \n");
+			_exit(EXIT_FAILURE);
+		}
+
+                ret = mount(NULL, "/", NULL, MS_SLAVE|MS_REC, NULL);
+                if (ret < 0) {
+                        ret = -errno;
+                        printf("mount() failed: %d (%m)\n", ret);
+                        _exit(EXIT_FAILURE);
+                }
+
+                ret = setup_and_test_procfs();
+                if (ret < 0) {
+                        printf("failed at procfs test\n");
+                        _exit(EXIT_FAILURE);
+                }
+                        
+                /* TODO: test here stats and other uidshift results */
+                execle("/bin/sh", "-sh", NULL, NULL);
+        }
+
+	ret = eventfd_write(efd, pid);
+	if (ret < 0) {
+		ret = -errno;
+		printf("error eventfd_write(): %d (%m)\n", ret);
+		return ret;
+	}
+
+        rpid = waitpid(pid, &status, 0);
+        if (rpid < 0) {
+                ret = -errno;
+                printf("waitpid() failed: %d (%m)\n", ret);
+                return ret;
+        } 
+
+        if (rpid != pid) {
+		printf("waited for %d got %d\n", pid, rpid);
+                return -1;
+        }
+
+        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+                printf("child did not terminate cleanly\n");
+                return -1;
+        }
+
+        return 0;
+}
+
+static void nop_handler(int sig) {}
+
+static int test_uidshift_mount(void)
+{
+        int ret;
+        int status;
+        pid_t pid, rpid;
+        struct sigaction oldsa;
+        struct sigaction sa = {
+                .sa_handler = nop_handler,
+                .sa_flags = SA_NOCLDSTOP,
+        };
+	eventfd_t event_status = 0;
+
+        efd = eventfd(0, EFD_CLOEXEC);
+        if (efd < 0) {
+                ret = -errno;
+                printf("eventfd() failed: %d (%m)\n", ret);
+                return ret;
+        }
+
+        efd_userns_child = eventfd(0, EFD_CLOEXEC);
+        if (efd_userns_child < 0) {
+                ret = -errno;
+                printf("eventfd() failed: %d (%m)\n", ret);
+                return ret;
+        }
+
+	ret = sigaction(SIGCHLD, &sa, &oldsa);
+	if (ret < 0) {
+		ret = -errno;
+		printf("sigaction() failed: %d (%m)\n", ret);
+		return ret;
+	}
+
+	pid = syscall(__NR_clone, SIGCHLD|CLONE_NEWNS, NULL);
+	if (pid < 0) {
+                ret = -errno;
+		printf("clone() failed: %d (%m)\n", -errno);
+		return ret;
+	}
+
+	if (pid == 0) {
+		ret = prctl(PR_SET_PDEATHSIG, SIGKILL);
+		if (ret < 0) {
+			ret = -errno;
+			printf("error prctl(): %d (%m)\n", ret);
+			_exit(EXIT_FAILURE);
+		}
+
+		/* reset sighandlers of childs */
+		ret = sigaction(SIGCHLD, &oldsa, NULL);
+		if (ret < 0) {
+			ret = -errno;
+			printf("sigaction() failed: %d (%m)\n", ret);
+			_exit(EXIT_FAILURE);
+		}
+
+                ret = mount(NULL, "/", NULL, MS_SLAVE|MS_REC, NULL);
+                if (ret < 0) {
+                        ret = -errno;
+                        printf("mount() failed: %d (%m)\n", ret);
+                        _exit(EXIT_FAILURE);
+                }
+
+                ret = outer_child();
+                _exit(ret);
+        }
+
+	ret = eventfd_read(efd, &event_status);
+	if (ret < 0) {
+                ret = -errno;
+		printf("error eventfd_read()\n");
+		return ret;
+	}
+
+        ret = setup_userns(event_status);
+        if (ret < 0) {
+                ret = -errno;
+                printf("error mapping uid and gid in userns\n");
+                return ret;
+        }
+
+	ret = eventfd_write(efd_userns_child, 1);
+	if (ret < 0) {
+		ret = -errno;
+		printf("error eventfd_write(): %d (%m)\n", ret);
+		return ret;
+	}
+
+        rpid = waitpid(pid, &status, 0);
+        if (rpid < 0) {
+                ret = -errno;
+                printf("waitpid() failed: %d (%m)\n", ret);
+                return ret;
+        }
+
+        if (rpid != pid) {
+		printf("waited for %d got %d\n", pid, rpid);
+                return -1;
+        }
+
+        close(efd);
+        close(efd_userns_child);
+
+        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+                printf("child did not terminate cleanly\n");
+                return -1;
+        }
+
+        return 0;
+}
+
+int main(int argc, char **argv)
+{
+        if (test_uidshift_mount() < 0) {
+                printf("uidshift mounting test failed\n");
+                exit(EXIT_FAILURE);
+        }
+
+	return EXIT_SUCCESS;
+}
